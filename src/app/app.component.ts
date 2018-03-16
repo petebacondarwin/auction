3f7c84629d0dba9d217c51b7f436e4a0e0545a27
Pete Bacon Darwin <pete@bacondarwin.com>
2018-03-16 17:03:12 +0000

    initial firestore implementation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from 'angularfire2/firestore';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+
+interface Category {
+  name: string
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit{
+
+  categoriesCol: AngularFirestoreCollection<Category>;
+  categories: Observable<Category[]>;
+
+  constructor(private afs: AngularFirestore) {}
+
+  ngOnInit() {
+    this.categoriesCol = this.afs.collection('categories');
+    this.categories = this.categoriesCol.valueChanges();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
 
 import { AppComponent } from './app.component';
 
+const firebaseConfig = {
+  apiKey: "AIzaSyB9hEev-uMrgTQilyuHCtKcEUFMcVOFcpM",
+  authDomain: "coleridge-summer-fair.firebaseapp.com",
+  databaseURL: "https://coleridge-summer-fair.firebaseio.com",
+  projectId: "coleridge-summer-fair",
+  storageBucket: "coleridge-summer-fair.appspot.com",
+  messagingSenderId: "1052904504519"
+};
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFirestoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
