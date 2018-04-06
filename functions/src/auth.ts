9c64c78befba74552a28402f4c7fd0c83844c0e6
Pete Bacon Darwin <pete@bacondarwin.com>
2018-04-06 10:47:42 +0100

    update assets

--- functions/src/auth.ts
+++ functions/src/auth.ts
@@ -4,5 +4,5 @@ import { sendEmail } from './email';
 export const sendWelcomeEmail = auth.user()
   .onCreate(async (event: Event<any>) => {
     const user = event.data;
-    await sendEmail(user.email, `Welcome to the Coleridge Summber!`, `Hey ${user.displayName || ''}!\nWelcome to the Coleridge Summer Fair.`);
+    await sendEmail(user.email, `Welcome to the Coleridge Summer Fair!`, `Hi ${user.displayName || ''}!\nWelcome to the Coleridge Summer Fair.`);
   });
